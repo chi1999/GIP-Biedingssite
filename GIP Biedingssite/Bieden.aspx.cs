80b51530c05f59b1446b1ed63b338c18bfc4a92d
JoachimNoe <joachim.noe@imb-brugge.net>
2018-01-24 10:00:28 +0100

    Bieding

--- GIP Biedingssite/Bieden.aspx.cs
+++ GIP Biedingssite/Bieden.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,8 @@ namespace GIP_Biedingssite
 {
     public partial class WebForm4 : System.Web.UI.Page
     {
-        public static string strconnectie = ConfigurationManager.ConnectionStrings["Appel"].ConnectionString;
+        
+        public static string strconnectie = ConfigurationManager.ConnectionStrings["gip"].ConnectionString;
 
         public static OleDbConnection cnn = new OleDbConnection(strconnectie);
         protected void Page_Load(object sender, EventArgs e)
@@ -20,7 +22,10 @@ namespace GIP_Biedingssite
         }
         protected void Bieden(object sender, EventArgs e)
         {
-
+            string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
+            // Get the IP  
+            string myIP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            txtBod.Text = myIP;
             OleDbCommand cmd = new OleDbCommand();
 
             cmd.Connection = cnn;
