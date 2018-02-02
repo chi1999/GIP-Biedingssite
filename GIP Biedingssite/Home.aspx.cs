4ed9e6aeb0f45d2286c93398b1353368520facbe
Richard Salens <richard.salens@imb-brugge.net>
2018-02-02 10:08:29 +0100

    home

--- GIP Biedingssite/Home.aspx.cs
+++ GIP Biedingssite/Home.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+
 
 namespace GIP_Biedingssite
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+        public static string strconnectie = ConfigurationManager.ConnectionStrings["gip"].ConnectionString;
+        public static OleDbConnection cnn = new OleDbConnection(strconnectie);
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -26,6 +32,24 @@ namespace GIP_Biedingssite
             pnlLogin.Visible = false;
             pnlregistr.Visible = true;
 
+        }
+
+        protected void registreren(object sender, EventArgs e)
+        {
+            
+
+            OleDbCommand  cmdRegistreren = new OleDbCommand();
+
+            cmdRegistreren.Connection = cnn;
+
+            cmdRegistreren.CommandText = cnn;
+
+            string strSQL;
+            strSQL = "gip";
+
+            cmd.CommandText = strSQL;
+            cnn.Open();
+            cnn.Close();
         }
     }
 }
