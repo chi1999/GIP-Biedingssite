3b6f5d1b09761df6e7e922c9285f32cd2326fd66
JoachimNoe <joachim.noe@imb-brugge.net>
2018-02-19 09:53:37 +0100

    idk

--- GIP Biedingssite/ArtikelenLeerlingen.aspx.cs
+++ GIP Biedingssite/ArtikelenLeerlingen.aspx.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+
 
 namespace GIP_Biedingssite
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+        public static string strconnectie = ConfigurationManager.ConnectionStrings["gip"].ConnectionString;
+        public static OleDbConnection cnn = new OleDbConnection(strconnectie);
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            gdvArtikelenLeerling.Visible = true;
+        }
 
+        protected void dgvArtikelenLeerling_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["ArtikelID"]= gdvArtikelenLeerling.SelectedRow.Cells[1].Text;
+            Server.Transfer("Bieden.aspx");
         }
     }
 }
